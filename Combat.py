1fb3e9e6a585115342e2a8d78479eb2777d81cfb
Arnaud Sallafranque <arnaud.sallafranque@labom2iformation.fr>
2022-08-17 15:26:01 +0200

    Suppression fichiers inutiles

--- Combat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import creerMonstre.py, creerPerso.py, Degat.py
-
-def GestionCombat(Perso, Monstre):
-    while Perso[1] > 0 and Monstre[1] > 0:
-        Monstre = GestionDegats(Perso, Monstre)
-        Perso = GestionDegats(Monstre, Perso)
-    return Perso
